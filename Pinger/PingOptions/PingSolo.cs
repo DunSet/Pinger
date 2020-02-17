d2361cdab01c04f359d1c0bcc5bc07e15d067576
Yaroslav <afanasiev_y@rusklimat.ru>
2020-02-17 15:35:12 +0300

    Add interface and implementation on methods

---
+++ Pinger/PingOptions/PingSolo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using Pinger.Intefaces;
+
+namespace Pinger
+{
+    class PingSolo : IPing
+    {
+       
+        void IPing.Ping(int a, int b, int c, int d)
+        {
+           
+        }
+    }
+}
